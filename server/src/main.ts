a538d158fecaef1ff905d5c3e6f629fb936b7e67
shfur2006 <shfur2006@npixel.co.kr>
2025-11-28 15:49:55 +0900

    fix: allow production domain in cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000', // Next.js 클라이언트 주소
+    origin: [
+      process.env.CLIENT_URL || 'http://localhost:3000',
+      'http://shop.moibluu.com:3000',
+      'http://www.shop.moibluu.com:3000'
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 4000);
